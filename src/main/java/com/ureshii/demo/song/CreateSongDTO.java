3e25f6143c43d28c7bda7b82db564c0be6f1623d
Mohammad Asheri <mhm.asheri@gmail.com>
2023-07-23 14:54:07 +0330

    WIP

--- src/main/java/com/ureshii/demo/song/CreateSongDTO.java
+++ src/main/java/com/ureshii/demo/song/CreateSongDTO.java
@@ -8,7 +8,7 @@ public record CreateSongDTO(String name, String language, String bitrate, Long a
                             Long duration,
                             @Size(min = 20000, max = 6000000, message = "You should upload image with size in range(10" +
                                     "K-6M)")
-                            byte[] pictureBytes,
+                            byte[] pictureBytes ,
                             @Size(min = 20000, max = 6000000, message = "You should upload image with size in range(10" +
                                     "K-6M)")
                             byte[] songBytes) {
